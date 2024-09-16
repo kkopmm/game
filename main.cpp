228d239d634027bcd678639e84934346a44ff5bc
kkopmm <dabaobeix@gmail>
2024-09-16 14:18:30 +0800

    fix bug

--- main.cpp
+++ main.cpp
@@ -2,14 +2,25 @@
 #include "resources_manager.h"
 
 ResourcesManager *res_manager = ResourcesManager::instance();
-
+bool music_on = true;
 int main()
 {
 	loadimage1();
 	srand((unsigned)time(NULL));
-	initgraph(width, height);
+	HWND hwnd = initgraph(width, height);
 	int a = 1;
-
+	// TODO: 初始化
+	try
+	{
+		res_manager->load();
+	}
+	catch (const LPCTSTR id)
+	{
+		TCHAR error_msg[256];
+		wsprintf(error_msg, _T("资源加载失败: %s"), id);
+		MessageBox(hwnd, error_msg, _T("错误"), MB_OK | MB_ICONERROR);
+		return -1;
+	}
 	while (1)
 	{
 		switch (a)
