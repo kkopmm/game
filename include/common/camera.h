67bed45730d62041729cd79a7edf0444c7145f97
kkopmm <dabaobeix@gmail>
2024-09-17 22:35:40 +0800

    新增相机类, 实现基础功能

---
+++ include/common/camera.h
@@ -0,0 +1,28 @@
+#ifndef _CAMERA_H
+#define _CAMERA_H
+
+#include "vector2.h"
+
+class Camera
+{
+public:
+    Camera() = default;
+    ~Camera() = default;
+    const Vector2 &get_position() const
+    {
+        return position;
+    }
+    void reset()
+    {
+        position.x = 0;
+        position.y = 0;
+    }
+    void on_update(float delta)
+    {
+    }
+
+private:
+    Vector2 position = {0, 0};
+};
+
+#endif // _CAMERA_H
