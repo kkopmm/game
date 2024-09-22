2d2098e4461fa0db439800ea8de4173d1314c417
kkopmm <dabaobeix@gmail>
2024-09-22 21:55:39 +0800

    添加胜利音乐

--- include/scene/victory_scene.h
+++ include/scene/victory_scene.h
@@ -2,6 +2,7 @@
 #define _VICTORY_SCENE_H
 
 #include "scene.h"
+#include "util.h"
 #include "resources_manager.h"
 
 extern ResourcesManager *res_manager;
@@ -9,10 +10,14 @@ extern ResourcesManager *res_manager;
 class VictoryScene : public Scene
 {
 public:
-    void on_enter() {};
+    void on_enter()
+    {
+        play_audio(L"胜利音乐");
+    };
     void on_exit() {};
     void on_update(float delta) {};
-    void on_draw() {
+    void on_draw()
+    {
         putimage(0, 0, res_manager->get_image("通关画面"));
     };
     void on_input(const ExMessage &msg) {};
